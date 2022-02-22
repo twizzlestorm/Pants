dad057a40c2e405b7303e354e9b26c21536ba434
Ethan <twizzlestorm@gmail.com>
2022-02-22 13:20:25 -0500

    Initial
    hi

---
+++ src/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('yt-navigate-start', process);
+if (document.body) process();
+else document.addEventListener('DOMContentLoaded', process);
+	
+function process() {
+	let s = window.location.href;
+
+	if ((s.substring(0, 30)) == 'https://www.youtube.com/shorts') {
+		window.location.replace('https://www.youtube.com/watch?v=' + s.substring(31));
+	}
+}
