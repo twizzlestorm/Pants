44d59ddfd851083a182d795afa7bd0e823c9a5f0
Ethan <twizzlestorm@gmail.com>
2022-02-23 14:34:28 -0500

    ryan3/3

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ else document.addEventListener('DOMContentLoaded', process);
 function process() {
 	let s = window.location.href;
 
-	if ((s.substring(0, 30)) == 'https://www.youtube.com/shorts') {
-		window.location.replace('https://www.youtube.com/watch?v=' + s.substring(31));
+	if ((s.substring(24, 30)) == 'shorts') {
+		window.location.replace(`https://www.youtube.com/watch?v=${s.substring(31)}`);
 	}
 }
